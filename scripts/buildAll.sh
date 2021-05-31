019a220baa1d5832585b5d9b8b0df747010ba30e
Henry Ye <yehenry11@gmail.com>
2021-05-31 14:41:47 -0400

    Changed small scripts

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -2,8 +2,8 @@ cd ~/workspace/gemini-engine/build
 cmake .. && make
 
 cd ../gemini/shaders/
-glslangValidator -V shader.vert -o vert.spv
-glslangValidator -V shader.frag -o frag.spv
+glslangValidator -V shader.vert -o shader.vert.spv
+glslangValidator -V shader.frag -o shader.frag.spv
 
 cd ../../
 g++ -g -std=c++17 ./app/*.cpp -L./build/ -lGemini -lvulkan -lglfw -I./gemini/ -I./ -o ./app.run
